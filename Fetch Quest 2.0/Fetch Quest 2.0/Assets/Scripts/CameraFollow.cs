8ae4f042c6177b3a0aa43b74ad9cd4f34db959f0
OllieSAE <1020925@student.sae.edu.au>
2021-11-17 17:48:08 +1100

    animated button, initial build for itch

--- Fetch Quest 2.0/Fetch Quest 2.0/Assets/Scripts/CameraFollow.cs
+++ Fetch Quest 2.0/Fetch Quest 2.0/Assets/Scripts/CameraFollow.cs
@@ -11,7 +11,6 @@ public class CameraFollow : MonoBehaviour
 
     void FixedUpdate()
     {
-        //can we lock the camera to the map, so it DOESN'T follow the player when they fall to a gruesome death?
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
